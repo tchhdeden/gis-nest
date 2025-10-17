5ed6e8ed70098ccc9e5ca01474910ad01688c94f
long127 <long127@gmail.com>
2025-10-17 17:38:18 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoryModule } from './category/category.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, CategoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/category/category.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class CategoryService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(createCategoryDto: CreateCategoryDto) {
+    return this.prisma.category.create({
+      data: createCategoryDto,
+    });
+  }
+
+  async findAll() {
+    return this.prisma.category.findMany();
+  }
+
+  async findOne(id: string) {
+    return this.prisma.category.findUnique({
+      where: { id },
+    });
+  }
+
+  async update(id: string, updateCategoryDto: UpdateCategoryDto) {
+    return this.prisma.category.update({
+      where: { id },
+      data: updateCategoryDto,
+    });
+  }
+
+  async remove(id: string) {
+    return this.prisma.category.delete({
+      where: { id },
+    });
+  }
+}
